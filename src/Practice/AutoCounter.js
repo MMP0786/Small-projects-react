e8697cd9f7c6ff99de7c174fdf245cb8a0de8adf
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-04-23 17:21:41 +0530

    Auto Counter Update

--- src/Practice/AutoCounter.js
+++ src/Practice/AutoCounter.js
@@ -3,18 +3,26 @@ import React, { useEffect, useState } from 'react'
 function AutoCounter() {
     const [count, setCount] = useState(0)
 
-    useEffect(()=>{
-        setInterval(() => {
-           setCount((count)=>{
-            return count +1
-           }) 
-        }, 1000);
-        // setInterval(() => setCount(count=> count+1) , 2000);
-    },[])
+    const start = ()=>{
+
+     let intervel=  setInterval(() => {
+         setCount((count)=>{
+          return count + 1
+         }) 
+      }, 1000);
+      document.querySelector(".start").setAttribute("disabled", true)
+
+      document.querySelector(".stop").addEventListener("click", ()=>{
+        clearInterval(intervel)
+        console.log(intervel)
+      })
+    }
+    
   return (
     <div>
         <p>{count}</p>
-        <button>Start</button>
+        <button onClick={start} className='start'>Start</button>
+        <button className='stop'>Stop</button>
         
     </div>
   )
