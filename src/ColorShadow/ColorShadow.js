e538f122316ec540e934cd274ae37e3bf11811f3
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-05-02 17:24:29 +0530

    completed

--- src/ColorShadow/ColorShadow.js
+++ src/ColorShadow/ColorShadow.js
@@ -3,16 +3,19 @@ import "./ColorShadow.css"
 
 function ColorShadow() {
   const [hori, setHori] = useState(0)
-  const [ver, setVer] = useState(0)
+  const [ver, setVer] = useState(10)
   const [blur, setBlur] = useState(0)
+  const [color, setColor]= useState("black")
   return (
     <div className='containor'>
         <div>
-            <div><input type="range" name="" id="" min={-200} max={200} /> </div>
-            <div><input type="range" name="" id="" min={-200} max={200} /> </div>
-            <div><input type="range" name="" id="" min={-200} max={200} /> </div>
+            <div><input type="range" name="" id="" value={hori} onChange={(e)=> setHori(e.target.value)} min={-20} max={20} /> </div>
+            <div><input type="range" name="" id="" value={ver} onChange={(e)=> setVer(e.target.value)} min={-20} max={20} /> </div>
+            <div><input type="range" name="" id="" value={blur} onChange={(e)=> setBlur(e.target.value)} min={0} max={50} /> </div>
+            <div><input type="color" name="" id="" value={color} onChange={(e)=> setColor(e.target.value)} min={0} max={50} /> </div>
+
         </div>
-        <div>box shadow</div>
+        <div className='box' style={{boxShadow:`${hori}px ${ver}px ${blur}px ${color}`}}>box shadow</div>
     </div>
   )
 }
