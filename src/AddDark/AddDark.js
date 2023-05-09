08b2bb29345661a056aa48d57f562c8dbd5ff690
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-05-09 22:20:01 +0530

    dark Mode

---
+++ src/AddDark/AddDark.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AddDark() {
+  return (
+    <div>
+         <h2>Dark Mode</h2>
+         <div>This is Dark Mode</div>
+    </div>
+  )
+}
+
+export default AddDark
