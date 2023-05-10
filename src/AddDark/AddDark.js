0645b64ab5964f31819760660c3db038b9ba11f3
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-05-10 20:28:06 +0530

    initial commit

--- src/AddDark/AddDark.js
+++ src/AddDark/AddDark.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Switch } from '@mui/material'
 
 function AddDark() {
   return (
     <div>
+        <Switch checked={offset} onChange={()=>setOffset(!offset)}  defaultChecked />
          <h2>Dark Mode</h2>
          <div>This is Dark Mode</div>
     </div>
